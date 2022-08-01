858df4c3ddbf20ed4b1d55ddac793ffc60b363b6
Nicholas Avino <nckavino0903@gmail.com>
2022-08-01 12:27:17 -0400

    commit tree 130f036504864aac950dadca78918d8af6170064

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return "This one is on me!";
+  }
+  else if (400 < feet , feet <= 2000) {
+    return 'That will be twenty bucks.';
+  }
+  else if (feet > 2000 , feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (feet > 2500) {
+    return 'No can do.';
+  }
 }
 
 function ternaryCheckCity(){
